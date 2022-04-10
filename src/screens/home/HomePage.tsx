cc9f8916ebd776ac50e291051a8ad56c7c789400
devpenzil <ajoalex012@gmail.com>
2022-04-10 19:23:28 +0530

    side bar

--- src/screens/home/HomePage.tsx
+++ src/screens/home/HomePage.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import AsideBar from '../../components/asidebar/AsideBar'
-import './index.scss'
+import React from "react";
+import AsideBar from "../../components/asidebar/AsideBar";
+import "./index.scss";
+import Home from "./routes/Home";
 
 function HomePage() {
   return (
-    <div className='flex w-full h-screen home'>
-        <div>
-            <AsideBar />
-        </div>
-        <div className='w-full'>
-            gfdgh
-        </div>
+    <div className="flex w-full h-screen home">
+      <div>
+        <AsideBar />
+      </div>
+      <div className="w-full p-4">
+        <Home />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
